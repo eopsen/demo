8504d10b6f3f8adede8bf88dc369ba57fe598a34
maciej <mb@4sys.pl>
2022-05-14 19:11:58 +0200

    added example route

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +13,14 @@ public class HomeController {
     @ResponseBody
     public String index() {
         return "Hello World!";
+    }
+
+
+
+    @GetMapping(value = "/example", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String getHTML() {
+        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+                "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
     }
 }
